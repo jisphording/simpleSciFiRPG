b00da9e79d010339da778a6632e3461ece340c03
Johannes Isphording <mail@johannesisphording.de>
2019-03-03 01:51:33 +0100

    [phaser] boot file updated
    boot file calls fewer resources and displays just the game title screen, then hands of to the mainLoop

--- src/scenes/boot.js
+++ src/scenes/boot.js
@@ -1,7 +1,6 @@
 // BOOT SCENE
 // -------------------------------------------------------------
 // Here we prepare the barebone elements for startup and greet the player with a Boot-/Intro-/Startup screen while the game continues loading in the background.
-
 export class BootScene extends Phaser.Scene {
 
     // the unique scene identifier. 
@@ -12,39 +11,28 @@ export class BootScene extends Phaser.Scene {
 
     preload ()  {
         // Load all assets // nameKey - src
-        this.load.image('bg', 'assets/images/bg/gradient.jpg');
+        this.load.image('screenTitle', 'assets/images/screens/title.png');
     }
 
     create () {
-        // adding a beautiful start screen background
-        this.add.image(320, 240, 'bg');
+        // Make it topmost in the beginning
+        this.scene.bringToTop('BootScene');
 
-        var fontColor = '#00ff00';
-        var fontDefinition = "32px Courier";
-        
-        // Here we display the name ofthe game
-        var nameLabel = this.add.text(10, 100, 'simpleSciFiRPG', { font: fontDefinition, fill: fontColor });
-
-        // Player instructions on how to start the game
-        var startLabel = this.add.text(10, 150, 'press the "W" key to start', { font: fontDefinition, fill: fontColor });
+        this.add.image(320, 240, 'screenTitle');
         
         // Making the 'W' key for pressing available
-        // the following is quite hacky at the moment. I do not now the proper JS way of referencing 'this' when it changes. since the scene transition method lives on the BootScene (here = 'this') i save it ti a variable that, because when going into the following keydown function 'this' changes to that function I I do not know yet how to properly get a hold back onto the Scene Object from within the keyboard event to call the transition method.
+        // the following is quite hacky at the moment. I do not now the proper JS way of referencing 'this' when it changes. since the scene transition method lives on the BootScene (here = 'this') i save it as a variable that, because when going into the following keydown function 'this' changes to that function I I do not know yet how to properly get a hold back onto the Scene Object from within the keyboard event to call the transition method.
         let that = this;
 
         this.input.keyboard.on('keydown_W', function (event) {
             that.transition();
+            let element = document.getElementsByClassName('game-detail');
+            element[0].innerHTML = '<p class="alert-green">Yeah, the game is running!</p>';    
         });
     }
 
     transition () {
-        console.log('Transition');
-        this.scene.transition({
-            target: 'GameScene',
-            duration: 3000,
-            moveBelow: true,
-            onUpdate: this.transitionOut,
-            data: { x: 640, y: 480 }
-        });
+        console.log('Transition to starting the game!');
+        this.scene.start('MainLoop');
     }
 }
