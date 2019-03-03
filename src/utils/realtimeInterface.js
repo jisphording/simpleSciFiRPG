20df9e8b029176a1a72742c190aed579eb21c67c
Johannes Isphording <mail@johannesisphording.de>
2019-03-03 01:53:43 +0100

    [phaser] realtimeinterface
    A seperate, central file for the user interface "inside phaser canvas" was created

---
+++ src/utils/realtimeInterface.js
@@ -0,0 +1,16 @@
+// REALTIME INTERFACE
+// -------------------------------------------------------------
+// Most of the user interface for this game will be realised DOM-based via HTML and CSS, but for the use cases where realtime interface feedback is needed we define all variables in this file
+
+export class RealtimeInterface {
+
+    static colors () {
+        let fontColor = '#00ff00';
+        return fontColor;
+    }
+
+    static fontBig () {
+        let fontDefinition = "72px Courier";
+        return fontDefinition;
+    }
+}
