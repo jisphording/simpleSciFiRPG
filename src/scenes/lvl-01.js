436d8c08b070b9c8105bfacb1615bc14c458025c
Johannes Isphording <mail@johannesisphording.de>
2019-03-05 23:45:25 +0100

    [Lvl-01] added Tilemap
    A basic Tilemap with Tiled Map Editor was created and loaded into phaser

--- src/scenes/lvl-01.js
+++ src/scenes/lvl-01.js
@@ -13,6 +13,10 @@ export class Lvl01 extends Phaser.Scene {
 
     preload () {
         // load all assets // nameKey - src
+        // loading the tile-sheet itself
+        this.load.image('environment-tiles', 'assets/images/tiles/environment-tiles.png');
+        // loading the json data for the map created with Tiled Editor
+        this.load.tilemapTiledJSON('lvl-01-map', 'assets/maps/lvl-01-map.json');
     }
 
     create () {
@@ -20,14 +24,16 @@ export class Lvl01 extends Phaser.Scene {
         let colors = RealtimeInterface.colors();
         let fontBig = RealtimeInterface.fontBig();
 
-        // creating a recognizable graphic to better visualize which scene is active
-        let graphics = this.add.graphics();
+        // Creating the level from the tilemap - first pulling the json from above
+        this.tileMap = this.add.tilemap('lvl-01-map');
+        // Then connecting the json map from tiled with the tile-sheet image preloaded in phaser
+        this.tileSet = this.tileMap.addTilesetImage('lvl-01-map', 'environment-tiles');
+        // Creating our Layers by assigning their keys/names from Tiled editor, starting with the background layer
+        this.backgroundLayer = this.tileMap.createStaticLayer('ground-walkable', this.tileSet);
+        // Then adding additional layers // The X, Y here is starting from the top left corner
+        this.tileMap.createStaticLayer('ground-impassable', this.tileSet, 0, 0);
 
-        graphics.fillStyle(0xff3300, 1);
-
-        graphics.fillRect(0, 100, 640, 480);
-        graphics.fillRect(25, 35, 85, 85);
-
+        // Adding a navigation helper
         this.add.text(25, 35, 'A', { font: fontBig, fill: colors });
 
         // I need a reference to this in the following function but do not know the proper way of achieving this yet
