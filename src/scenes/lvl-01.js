426e310e4f41cca54a7f6cac8aff0fe9b3e9e67d
Johannes Isphording <mail@johannesisphording.de>
2019-03-16 00:12:10 +0100

    [lvl-01] placing a first map
    Importing a map created with Tiled and loading it's layers.

--- src/scenes/lvl-01.js
+++ src/scenes/lvl-01.js
@@ -24,17 +24,20 @@ export class Lvl01 extends Phaser.Scene {
         let colors = RealtimeInterface.colors();
         let fontBig = RealtimeInterface.fontBig();
 
+        // Adding a navigation helper
+        this.add.text(25, 35, 'A', { font: fontBig, fill: colors });
+
         // Creating the level from the tilemap - first pulling the json from above
         this.tileMap = this.add.tilemap('lvl-01-map');
         // Then connecting the json map from tiled with the tile-sheet image preloaded in phaser
-        this.tileSet = this.tileMap.addTilesetImage('lvl-01-map', 'environment-tiles');
+        this.tileSet = this.tileMap.addTilesetImage('environment-tiles', 'environment-tiles');
         // Creating our Layers by assigning their keys/names from Tiled editor, starting with the background layer
         this.backgroundLayer = this.tileMap.createStaticLayer('ground-walkable', this.tileSet);
         // Then adding additional layers // The X, Y here is starting from the top left corner
         this.tileMap.createStaticLayer('ground-impassable', this.tileSet, 0, 0);
-
-        // Adding a navigation helper
-        this.add.text(25, 35, 'A', { font: fontBig, fill: colors });
+        this.tileMap.createStaticLayer('item-crates', this.tileSet, 0, 0);
+        this.tileMap.createStaticLayer('player-start', this.tileSet, 0, 0);
+        this.tileMap.createStaticLayer('obstacle-pond', this.tileSet, 0, 0);
 
         // I need a reference to this in the following function but do not know the proper way of achieving this yet
         let that = this;
